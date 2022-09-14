1b7b35383ad35d706b5fa6b52fd4b9f0db69299a
Christopher Aneke <christopher@interneuron.org>
2022-09-14 20:38:17 +0200

    Add license to source files

--- E2eToDicomWorkerService/Program.cs
+++ E2eToDicomWorkerService/Program.cs
@@ -1,3 +1,9 @@
+/* Copyright (C) Interneuron, Inc - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Chukwuemezie Aneke <ccanekedev@gmail.com>, May 2020
+ */
+
 using System;
 using System.Collections.Generic;
 using System.Linq;
